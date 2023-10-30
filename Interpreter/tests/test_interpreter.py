d1f9ec8bc30ad20fa1143804c3dde46ae6ebb6d1
Zorin Egor <zorin-ep@mail.ru>
2023-10-30 17:29:55 +0800

    finish: cov == 100%

--- Interpreter/tests/test_interpreter.py
+++ Interpreter/tests/test_interpreter.py
@@ -45,6 +45,50 @@ class TestInterpreter:
 
     assert interpreter.eval("+1+-(-2)") == 3
 
+    def test_invalid_factor(self, interpreter):
+        with pytest.raises(SyntaxError):
+            assert interpreter.eval("11++")
+
+    def test_binop_invalid_operator(self, interpreter):
+        with pytest.raises(ValueError):
+            assert interpreter.visit_binop(BinOp(Number(Token(TokenType.NUMBER, "2")), Token(TokenType.OPERATOR, "&"), Number(Token(TokenType.NUMBER, "2"))))
+
+    def test_unop_invalid_operator(self, interpreter):
+        with pytest.raises(ValueError):
+            assert interpreter.visit_unop(UnOp(Token(TokenType.OPERATOR, "&"), Number(Token(TokenType.NUMBER, "2"))))
+
+    def test_ast_binop_str(self):
+        assert BinOp(Number(Token(TokenType.NUMBER, "2")), Token(TokenType.OPERATOR, "+"),
+                     Number(Token(TokenType.NUMBER, "2"))).__str__() == f"BinOp+ (Number (Token(TokenType.NUMBER, 2)), Number (Token(TokenType.NUMBER, 2)))"
+
+    def test_ast_unop_str(self):
+        assert UnOp(Token(TokenType.OPERATOR, "+"), Number(Token(TokenType.NUMBER, "2"))).__str__() == f"UnOp+ (Number (Token(TokenType.NUMBER, 2)))"
+
+    def test_nodeVisutor(self):
+        assert NodeVisitor().visit() is None
+
+    def test_add_with_spaces(self, interpreter):
+        assert interpreter.eval("2+ 2") == 4
+
+    def test_expr_break(self, interpreter):
+        assert interpreter.eval("2+2*2/2") == 6
+
+    def test_expr_continue(self, interpreter):
+        assert interpreter.eval("2+2*2/2+2") == 6
+
+    def test_invalid_expr(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("2+a")
+
+        with pytest.raises(SyntaxError):
+            interpreter.eval("t+a")
+
+    def test_invalid_token_order(self, parser):
+        parser._current_token = Token(TokenType.NUMBER, "2")
+        with pytest.raises(SyntaxError):
+            parser.check_token(TokenType.OPERATOR)
+
+
     def test_add_with_letter(self, interpreter):
         with pytest.raises(SyntaxError):
             interpreter.eval("2+a")
