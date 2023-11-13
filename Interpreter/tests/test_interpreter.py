835ebcb373ff48c039602ded099d67f5feab5052
Zorin Egor <zorin-ep@mail.ru>
2023-11-13 16:57:32 +0800

    add some test

--- Interpreter/tests/test_interpreter.py
+++ Interpreter/tests/test_interpreter.py
@@ -37,4 +37,50 @@ class TestInterpreter:
         with pytest.raises(SyntaxError):
             interpreter.eval("BEGIN a== 2 + 3 * (2 + 3); END.")
 
+    def test_bad_token_SEMI(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a= 2 + 3 * (2 + 3); END.")
 
+    def test_bad_token_DOT(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a= 2 + 3 * (2 + 3); END.")
+
+    def test_bad_token_END(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a= 2 + 3 * (2 + 3); END.")
+
+    def test_bad_token_ID(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a= 2 + 3 * (2 + 3); END.")
+
+    def test_bad_token_LPAREN(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a= 2 + 3 * (2 + 3); END.")
+
+    def test_bad_token_RPAREN(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a= 2 + 3 * (2 + 3); END.")
+
+
+    def test_invalid_token_order(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a = 2 + 3 * (2 + 3); END.")
+
+    def test_Invalid_statement(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a = 2 + 3 * (2 + 3); END.")
+
+    def test_Invalid_statement_assign(self, interpreter):
+        with pytest.raises(SyntaxError):
+            interpreter.eval("BEGIN a = 2 + 3 * (2 + 3); END.")
+
+    def test_ast_Assigment_num(self):
+        assert Assigment(Variable(Token(TokenType.ID, "h")), Number(Token(TokenType.NUMBER, "2"))).__str__() == \
+               f"Assigment Variable (Token(TokenType.ID, h)) (Number (Token(TokenType.NUMBER, 2)))"
+
+    def test_ast_Assigment_str(self):
+        assert Assigment(Variable(Token(TokenType.ID, "h")), Number(Token(TokenType.NUMBER, "2"))).__str__() == \
+               f"Assigment Variable (Token(TokenType.ID, h)) (Number (Token(TokenType.NUMBER, 2)))"
+
+    def test_ast_Empty(self):
+        assert Empty(Token(TokenType.END, "END.")).__str__() == "Empty (Token(TokenType.END, END.))"
