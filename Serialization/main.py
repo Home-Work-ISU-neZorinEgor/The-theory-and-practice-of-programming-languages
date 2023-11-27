cd60356ab417f2521a61d2d5a2f7f087ab926fcc
Zorin Egor <zorin-ep@mail.ru>
2023-11-27 21:52:30 +0800

    add example

---
+++ Serialization/main.py
@@ -0,0 +1,74 @@
+from enum import Enum
+import struct
+
+
+class Alignment(Enum):
+    HORIZONTAL = 1
+    VERTICAL = 2
+
+
+class Widget():
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.children = []
+        if self.parent is not None:
+            self.parent.add_child(self)
+
+    def add_child(self, child: "Widget"):
+        self.children.append(child)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}{self.children}"
+
+    def __repr__(self):
+        return str(self)
+
+
+class MainWindow(Widget):
+
+    def __init__(self, title: str):
+        super().__init__(None)
+        self.title = title
+
+
+class Layout(Widget):
+
+    def __init__(self, parent, alignment: Alignment):
+        super().__init__(parent)
+        self.alignment = alignment
+
+
+class LineEdit(Widget):
+
+    def __init__(self, parent, max_length: int = 10):
+        super().__init__(parent)
+        self.max_length = max_length
+
+class ComboBox(Widget):
+
+    def __init__(self, parent, items):
+        super().__init__(parent)
+        self.items = items
+
+
+app = MainWindow("Application")
+layout1 = Layout(app, Alignment.HORIZONTAL)
+layout2 = Layout(app, Alignment.VERTICAL)
+
+edit1 = LineEdit(layout1, 20)
+edit2 = LineEdit(layout1, 30)
+
+box1 = ComboBox(layout2, [1, 2, 3, 4])
+box2 = ComboBox(layout2, ["a", "b", "c"])
+
+print(app)
+
+bts = app.to_binary()
+print(f"Binary data length {len(bts)}")
+print(bts)
+
+new_app = MainWindow.from_binary(bts)
+print(new_app)
+
+print(new_app.children[1].children[1].items)
