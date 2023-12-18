f45fc1a6befdade46cf678c2cf5a00faede55789
Zorin Egor <zorin-ep@mail.ru>
2023-12-18 12:19:10 +0800

    update jar

--- zmq/src/main/java/org/example/Client.java
+++ zmq/src/main/java/org/example/Client.java
@@ -8,16 +8,9 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Client {
 
-    private static final java.util.Map<String, String> TABLES = Map.of(
-            "p", "photoresistor_data",
-            "b", "button_data",
-            "g", "gyroscope_data"
-    );
 
     public static void main(String[] args) {
         String serverAddress = "tcp://192.168.0.102:5555";
@@ -25,39 +18,35 @@ public class Client {
         try (ZMQ.Context context = ZMQ.context(1);
              ZMQ.Socket subscriber = context.socket(ZMQ.SUB)) {
 
-            // Подключение к серверу
+            // Connect to the server
             subscriber.connect(serverAddress);
 
-            // Установка подписки на все сообщения
+            // Subscribe to all messages
             subscriber.subscribe("".getBytes());
 
-            // Подготовка для сохранения логов в файл
+            // Prepare for saving logs to a file
             PrintWriter logWriter = new PrintWriter(new FileWriter("subscriber_logs.dat", true));
 
-            // Регулярное выражение для извлечения цифры
-            Pattern pattern = Pattern.compile("\\d+");
+            // Regular expression for extracting digits
+            // No need for a regex pattern in this modified code
 
             while (true) {
-                // Получение текущей даты и времени
+                // Get current date and time
                 LocalDateTime currentTime = LocalDateTime.now();
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-                // Получение сообщения
+                // Receive the message
                 String message = new String(subscriber.recv(0), ZMQ.CHARSET);
 
-                // Извлечение цифры из сообщения
-                Matcher matcher = pattern.matcher(message);
-                if (matcher.find()) {
-                    String digit = matcher.group();
+                // Print the entire received message to the console
+                System.out.println("[" + currentTime.format(formatter) + "] " + message);
 
-                    // Вывод в консоль с добавлением времени
-                    System.out.println("[" + currentTime.format(formatter) + "] " + digit);
+                // Extract the table name and digit from the message
+                String digit = message.replaceAll("\\D", ""); // Remove non-digits
 
-                    String tableName = TABLES.get(message.substring(0, 1));
-
-                    logWriter.println("[" + currentTime.format(formatter) + "] Table: " + tableName + ", Value: " + digit);
-                    logWriter.flush();
-                }
+                // Save logs to the file
+                logWriter.println("[" + currentTime.format(formatter) + "] Message: " + message);
+                logWriter.flush();
             }
         } catch (IOException e) {
             e.printStackTrace();
